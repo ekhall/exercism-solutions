dafb06cb11ea090f625119504403dbccfaa09588
E. Kevin Hall <hall@tengerine.com>
2017-01-09 09:50:15 -0500

    Initial setup

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -3,6 +3,8 @@
 (defn word-count [words]
   "Given a sentence, returns a map with a count of each word.
    The original word, as a string, is the key. The value is an
-   integer representing the number of occurences of this word.")
-  
+   integer representing the number of occurences of this word."
+  words)
+
+(word-count "words")
 
