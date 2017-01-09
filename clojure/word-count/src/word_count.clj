afd1c7bb44368e6b48f92b2675ba8d58d0629e53
E. Kevin Hall <hall@tengerine.com>
2017-01-09 09:48:20 -0500

    Initial

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,8 @@
 (ns word-count)
 
-(defn word-count []) ;; <- arglist goes here
-  ;; your code goes here
+(defn word-count [words]
+  "Given a sentence, returns a map with a count of each word.
+   The original word, as a string, is the key. The value is an
+   integer representing the number of occurences of this word.")
+  
 
