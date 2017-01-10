f4c7d119387b6f3bb6b3e1cc51800a78620786de
E. Kevin Hall <hall@tengerine.com>
2017-01-09 22:04:09 -0500

    Passing 3

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,16 +1,15 @@
 (ns word-count
-  (:require [clojure.string :refer [split]]))
+  (:require [clojure.string :refer [split lower-case]]))
 
 (defn word-count [words]
   "Given a sentence, returns a map with a count of each word.
    The original word, as a string, is the key. The value is an
    integer representing the number of occurences of this word."
-  (-> words
-      (split #" ")
-      (set)))
-      
+  (let [cleaned-string (apply str (re-seq #"[a-zA-Z\s]" (lower-case words)))
+        word-set (filter not-empty (-> cleaned-string (split #" ") (set)))
+        word-map (zipmap word-set (take (count word-set) (repeat 1)))]
+    word-map))
 
+(word-count "car : Carpet as java : javascript!!&@$%^&")
 
-
-(word-count "words are here")
 
